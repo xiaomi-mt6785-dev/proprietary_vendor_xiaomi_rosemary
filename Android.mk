672c4692a6c6674c80be270d4afe73415c55dda7
Matsvei Niaverau <matvej2002@gmail.com>
2024-02-19 13:00:09 +0100

    rosemary: Import firmware from `V14.0.7.0.TKLMIXM`

--- Android.mk
+++ Android.mk
@@ -6,4 +6,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),rosemary)
 
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,18031e6ae105754abc46fa12f420e465b8e7ccb9)
+$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,cc76b46f1c87a7082f416122049eb96c2cbffdbd)
+$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,3dd4b80ac5fc08785ae0650cf2c93e2148816ce6)
+$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,6f778d6a013bf1344dc9d5523d2239f4463c4a97)
+$(call add-radio-file-sha1-checked,radio/gz.img,d0f422d8065108334dd5c0187240e47da31dd127)
+$(call add-radio-file-sha1-checked,radio/lk.img,a20bb452beb717086eb4b1cd4af4d1416a53fd1c)
+$(call add-radio-file-sha1-checked,radio/logo.img,b63efa6ca11c805e6dcb4bd4bb65fca873a7ad53)
+$(call add-radio-file-sha1-checked,radio/md1img.img,a5bc030b2d6d110da1dd72b586d4e171559fcb05)
+$(call add-radio-file-sha1-checked,radio/scp.img,90b7fafaf3c5e39060b684b8db788c13b32fdd5d)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,ea7cfb679b22fea131fbe894932fb9590836e845)
+$(call add-radio-file-sha1-checked,radio/sspm.img,9ef7f3d01b3df910c6a3ac2406a4b98e394c952e)
+$(call add-radio-file-sha1-checked,radio/tee.img,6570165c7310c742445f34766eea2b07ee67e84f)
+
 endif
