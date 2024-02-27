88c0afdb49bc66b75a72176d163f1d6d10ca60e9
Matsvei Niaverau <matvej2002@gmail.com>
2024-02-27 08:48:50 +0100

    rosemary: Do not update `logo` partition

--- Android.mk
+++ Android.mk
@@ -12,7 +12,6 @@ $(call add-radio-file-sha1-checked,radio/cam_vpu2.img,3dd4b80ac5fc08785ae0650cf2
 $(call add-radio-file-sha1-checked,radio/cam_vpu3.img,6f778d6a013bf1344dc9d5523d2239f4463c4a97)
 $(call add-radio-file-sha1-checked,radio/gz.img,d0f422d8065108334dd5c0187240e47da31dd127)
 $(call add-radio-file-sha1-checked,radio/lk.img,a20bb452beb717086eb4b1cd4af4d1416a53fd1c)
-$(call add-radio-file-sha1-checked,radio/logo.img,b63efa6ca11c805e6dcb4bd4bb65fca873a7ad53)
 $(call add-radio-file-sha1-checked,radio/md1img.img,a5bc030b2d6d110da1dd72b586d4e171559fcb05)
 $(call add-radio-file-sha1-checked,radio/scp.img,90b7fafaf3c5e39060b684b8db788c13b32fdd5d)
 $(call add-radio-file-sha1-checked,radio/spmfw.img,ea7cfb679b22fea131fbe894932fb9590836e845)
