b1244c98f8bc753391bcb81e0afc9d1fd476ef4f
Matsvei Niaverau <matvej2002@gmail.com>
2024-03-01 14:04:14 +0100

    rosemary: Ship preloader as well

--- Android.mk
+++ Android.mk
@@ -13,6 +13,7 @@ $(call add-radio-file-sha1-checked,radio/cam_vpu3.img,6f778d6a013bf1344dc9d5523d
 $(call add-radio-file-sha1-checked,radio/gz.img,d0f422d8065108334dd5c0187240e47da31dd127)
 $(call add-radio-file-sha1-checked,radio/lk.img,a20bb452beb717086eb4b1cd4af4d1416a53fd1c)
 $(call add-radio-file-sha1-checked,radio/md1img.img,a5bc030b2d6d110da1dd72b586d4e171559fcb05)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,d79e645a8d415deb1386abbbe604206de03f5471)
 $(call add-radio-file-sha1-checked,radio/scp.img,90b7fafaf3c5e39060b684b8db788c13b32fdd5d)
 $(call add-radio-file-sha1-checked,radio/spmfw.img,ea7cfb679b22fea131fbe894932fb9590836e845)
 $(call add-radio-file-sha1-checked,radio/sspm.img,9ef7f3d01b3df910c6a3ac2406a4b98e394c952e)
