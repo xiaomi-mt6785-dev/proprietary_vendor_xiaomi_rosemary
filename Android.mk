0d1075013f49bd0ee23f9b6f4c3b8bbbbbe8ee86
Matsvei Niaverau <matvej2002@gmail.com>
2026-02-02 12:16:24 +0100

    rosemary: Ship kaeru as lk image

--- Android.mk
+++ Android.mk
@@ -11,7 +11,7 @@ $(call add-radio-file-sha1-checked,radio/cam_vpu1.img,217ec73ccd355f69ba22cf99d9
 $(call add-radio-file-sha1-checked,radio/cam_vpu2.img,c71ab5f25b9b44a943a199262af4c89847ed31bc)
 $(call add-radio-file-sha1-checked,radio/cam_vpu3.img,6d300741d92c2f7c7d76a211a054c38162f6ee71)
 $(call add-radio-file-sha1-checked,radio/gz.img,5afff9eefa64c2e24dc794ee8d5956d2594b78da)
-$(call add-radio-file-sha1-checked,radio/lk.img,6311dcbfe0fe22e3149c828d4ba61e6a6a917864)
+$(call add-radio-file-sha1-checked,radio/lk.img,3af188a85d1b44ac6c5bbe3ec6dff33c7be05ec1)
 $(call add-radio-file-sha1-checked,radio/md1img.img,06ffb17f82950ca938b3bf04a3907ea8bc9face6)
 $(call add-radio-file-sha1-checked,radio/preloader_raw.img,c963964e230ef975003fd1f790fe8952f18baa1b)
 $(call add-radio-file-sha1-checked,radio/scp.img,d795b777756f0665b9523dcf3fb6118db80c3289)
